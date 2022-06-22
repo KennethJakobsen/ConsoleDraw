8d6be3195d77b6b5e9768fd4e861ad1fa5e7dfca
Kenneth Jakobsen <kenneth.jakobsen@yahoo.com>
2022-06-22 08:48:32 +0200

    Initial commit

---
+++ SquareAndCross/Program.cs
@@ -0,0 +1,11 @@
+﻿for(var y = 0; y < 11; y++)
+{
+    for (var x = 0; x < 11; x++)
+    {
+        if (x == 0 || x == 10 || y == 0 || y == 10 || x==y || 10-x == y)
+            Console.Write("#");
+        else
+            Console.Write(" ");
+    }
+    Console.Write(Environment.NewLine);
+}
